b6bb863ab3fe1df0b390f410aab51da599dc67f4
Bartlomiej Styczen <styczen.bartlomiej@gmail.com>
2022-03-02 22:55:12 +0100

    Parsing command line arguments using "clap" crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    /// The programming language of GitHub repositories
+    #[clap(long = "language")]
+    language: String,
+
+    /// The amount of projects to be read from GitHub
+    #[clap(long = "project_count")]
+    project_count: u32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    let language = args.language;
+    let project_count = args.project_count;
+    println!(
+        "Programming language: {}, project count: {}",
+        language, project_count
+    );
 }
