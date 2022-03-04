6f764ccd260283a6c0b521ea8bc24660d13f9bbe
Bartlomiej Styczen <styczen.bartlomiej@gmail.com>
2022-03-04 23:48:47 +0100

    Figuring out how to pass authorization token to request

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+use std::io::Read;
+
 use clap::Parser;
-// use futures::executor::block_on;
-use http::{Request, Response};
 
 #[derive(Parser)]
 struct Args {
@@ -18,20 +18,34 @@ fn main() {
     let language = args.language;
     let project_count = args.project_count;
 
-    // Prepare URI for first request 
-    let req = format!(
+    // Prepare URL for first request
+    let req_url = format!(
         "https://api.github.com/search/repositories?q=language:{}&sort=stars&order=desc",
         language
     );
-    println!("{}", req);
+    println!("Request URI: {}", req_url);
 
-    // let mut request = Request::builder()
-    //     .uri("https://api.github.com/search/repositories?q=language:rust&sort=stars&order=desc")
-    //     .header("Accept", "application/vnd.github.v3+json");
+    let client = reqwest::blocking::Client::new();
+    let mut res = client
+        .get(req_url)
+        .header("Accept", "application/vnd.github.v3+json")
+        // .header("User-Agent", "styczen")
+        // .header(
+        //     "Authorization",
+        //     "token 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",
+        // )
+        .bearer_auth("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")
+        .send()
+        .unwrap();
 
-    // let request = Request::get(
-    //     "https://api.github.com/search/repositories?q=language:rust&sort=stars&order=desc",
-    // )
-    // .body(())
-    // .unwrap();
+    // let mut res = reqwest::blocking::get(req_uri).unwrap();
+    let mut body = String::new();
+    res.read_to_string(&mut body).unwrap();
+
+    // let links = res.headers().get(reqwest::header::LINK).unwrap();
+
+    println!("Status: {}", res.status());
+    println!("Headers: {:#?}", res.headers());
+    // println!("Body: {}", body);
+    // println!("Links: {:#?}", links);
 }
